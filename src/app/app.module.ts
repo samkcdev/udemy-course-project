28d7e6142690093eeb15be358a4bff0d84c0ff16
Sam Cherian <samnp87@gmail.com>
2019-04-03 23:21:48 +0530

    Created a new project based on Udemy course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { RecipesListComponent } from "./recipes/recipes-list/recipes-list.component";
+import { RecipesDetailsComponent } from "./recipes/recipes-details/recipes-details.component";
+import { RecipeItemComponent } from "./recipes/recipes-list/recipe-item/recipe-item.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe-model";
+
+@Component({
+  selector: "app-recipes-list",
+  templateUrl: "./recipes-list.component.html",
+  styleUrls: ["./recipes-list.component.scss"]
+})
+export class RecipesListComponent implements OnInit {
+  recipe: Recipe[] = [
+    new Recipe(
+      "Avial",
+      "Recipe for Avial-Kerala Special",
+      "https://myheartbeets.com/wp-content/uploads/2018/06/instant-pot-avial.jpg"
+    ),
+    new Recipe(
+      "Kichidi",
+      "Recipe for Kichidi",
+      "https://www.vegrecipesofindia.com/wp-content/uploads/2018/12/khichdi-recipe-1.jpg"
+    )
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+}
