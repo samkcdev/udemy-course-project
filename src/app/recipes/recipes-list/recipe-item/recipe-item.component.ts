83c4ca49b38fe63b0caeb1699e2a824e69c63302
Sam Cherian <samnp87@gmail.com>
2019-04-09 23:20:23 +0530

    Moved recipes from recipe item . Added binding using @Input,

--- src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import { Recipe } from "../../recipe-model";
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.scss']
+  selector: "app-recipe-item",
+  templateUrl: "./recipe-item.component.html",
+  styleUrls: ["./recipe-item.component.scss"]
 })
 export class RecipeItemComponent implements OnInit {
+  @Input("recipeItem") recipeItem: Recipe;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
